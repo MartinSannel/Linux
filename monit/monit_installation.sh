186d38ea2b79806fd22c22ff490cf810f9278c52
Daky <mail@sebfrodin.se>
2020-12-03 15:50:33 +0100

    Create monit_installation.sh

---
+++ monit/monit_installation.sh
@@ -0,0 +1,7 @@
+sudo cp /etc/monit/conf-available/apache2 /etc/monit/conf-enabled/apache2
+sudo cp /etc/monit/conf-available/mysql /etc/monit/conf-enabled/mysql
+sudo cp /etc/monit/conf-available/openssh-server /etc/monit/conf-enabled/openssh-server
+sudo touch /var/run/mysqld/mysqld.pid
+sudo chown mysql:mysql /var/run/mysqld/mysqld.pid
+printf "[mysqld]\npid-file = /var/run/mysqld.pid" >> /etc/mysql/my.cnf
+printf "set httpd port 2812 and\n    use address localhost\n    allow localhost" >> /etc/monit/monitrc
