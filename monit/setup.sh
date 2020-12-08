825b264b0e728d69e673829cef38361360993333
Daky <62486063+Daky60@users.noreply.github.com>
2020-12-08 15:57:56 +0100

    Update setup.sh

--- monit/setup.sh
+++ monit/setup.sh
@@ -3,5 +3,5 @@ sudo cp /etc/monit/conf-available/mysql /etc/monit/conf-enabled/mysql
 sudo cp /etc/monit/conf-available/openssh-server /etc/monit/conf-enabled/openssh-server
 sudo touch /var/run/mysqld/mysqld.pid
 sudo chown mysql:mysql /var/run/mysqld/mysqld.pid
-printf "[mysqld]\npid-file = /var/run/mysqld.pid" >> /etc/mysql/my.cnf
+printf "pid-file    = /var/run/mysqld/mysqld.pid" >> /etc/mysql/my.cnf
 printf "set httpd port 2812 and\n    use address localhost\n    allow seb:frodin" >> /etc/monit/monitrc
